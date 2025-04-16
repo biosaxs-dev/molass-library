f424bdfde9dfabaa64f7ba3069c336577c52e348
freesemt <freesemt@gmail.com>
2025-04-16 15:36:57 +0900

    add dependency on toml and update get_version() docstring

--- molass/__init__.py
+++ molass/__init__.py
@@ -4,7 +4,22 @@ import os
 def get_version(toml_only=False):
     """
     Retrieve the version of the package from pyproject.toml or importlib.metadata.
+
+    Parameters:
+    ------------
+    toml_only: bool, optional
+        If True, only read the version from pyproject.toml and ensure
+        that you are using the local repository. Default is False.
+        This is crucial when you are testing in environments you can also
+        use the installed package version.
+        In fact, while you are testing, it is almost always the case that
+        you are updating the local repository (not yet installed version),
+        and you will be confused unless you are certain which version you are using.
+
+        If False, read the version from pyproject.toml if it exists,
+        otherwise use importlib.metadata.
     """
+
     pyproject_toml = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'pyproject.toml')
     if os.path.exists(pyproject_toml):
         # If you are using the local repository, read the version from pyproject.toml
