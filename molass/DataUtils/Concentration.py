fd6bc7fcedd3dfae63e5ccdbb06c43436a61aeb3
freesemt <freesemt@gmail.com>
2025-07-11 17:33:33 +0900

    Add tests for v1-reporting

--- molass/DataUtils/Concentration.py
+++ molass/DataUtils/Concentration.py
@@ -33,5 +33,16 @@ def make_concinfo_impl(ssd, mapping, **kwargs):
         xr_curve = ssd.xr.get_icurve()
         uv_curve = ssd.uv.get_icurve()
         conc_curve = mapping.get_mapped_curve(xr_curve, uv_curve)
+        if debug:
+            import matplotlib.pyplot as plt
+            from molass.PlotUtils.TwinAxesUtils import align_zero_y
+            fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 5))
+            ax1.plot(uv_curve.x, uv_curve.y, label='UV')
+            ax2.plot(xr_curve.x, xr_curve.y, color='orange', label='XR')
+            axt = ax2.twinx()
+            axt.plot(conc_curve.x, conc_curve.y, label='Concentration')
+            align_zero_y(ax1, axt)
+            fig.tight_layout()
+            plt.show()
 
     return ConcInfo(conc_curve*concfactor)
